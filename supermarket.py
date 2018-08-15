7083243c7747beb5ad63cba7a68a3469bcd7b9b5
mjtedder <mjtedder@gmail.com>
2018-08-15 12:39:33 -0400

    create for loop for prices and stock.  printing to console successfully

--- supermarket.py
+++ supermarket.py
@@ -4,3 +4,15 @@ prices = {
   "orange": 1.5,
   "pear": 3
 }
+
+stock = {
+  "banana": 6,
+  "apple": 0,
+  "orange": 32,
+  "pear": 15
+}
+
+for food in prices:
+    print (food)
+    print ("Price: %s" % prices[food])
+    print ("Stock: %s" % stock[food])
